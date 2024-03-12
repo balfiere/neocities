a8df350ec1ebef68c682e92c8255b4e4194d587b
balfiere <deanleonheart@gmail.com>
2024-03-11 22:31:30 -0500

    move all to public

--- scripts/linkclick.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.sidecontent').hide();
-    $('.subheader').click(function() {
-        $(this).closest('.sidebox').find('.sidecontent').slideToggle(500);
-        return false;
-    });
-});
