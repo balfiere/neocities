a1e93e846bb86fc61331f18bfda07f5642f3a6f3
balfiere <deanleonheart@gmail.com>
2024-03-20 19:18:14 -0500

    fix footer and add dynamic padding

--- public/resources/scripts/collapsible.js
+++ public/resources/scripts/collapsible.js
@@ -12,3 +12,34 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+      $('#main').each(function(){ //loop through each element with the .dynamic-height class
+          $(this).css({
+              'padding-top' : header.offsetHeight + 10 +'px',
+              'padding-bottom' : footer.offsetHeight +'px'
+          });
+      });
+  } else {
+    $('#main').each(function(){ //loop through each element with the .dynamic-height class
+      $(this).css({
+          'padding' : '15px 12px 5px 0px',
+      });
+  });
+  }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 900px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+  myFunction(x);
+}); 
