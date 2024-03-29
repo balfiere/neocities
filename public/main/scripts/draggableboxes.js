7fc6192262ec9b903ca5a1c9ea30f1b1ae88ad2b
balfiere <deanleonheart@gmail.com>
2024-03-29 11:34:19 -0500

    bring currently clicked on window to top

--- public/main/scripts/draggableboxes.js
+++ public/main/scripts/draggableboxes.js
@@ -23,3 +23,27 @@ for (var i = 0, len = draggableElems.length; i < len; i++) {
 	});
 	draggies.push(draggie);
 }
+
+// get all windows
+var activeWindow = document.querySelector('.window_container');
+var windows = document.querySelectorAll(".window_container");
+
+var init = function(){
+	for (var i = 0; i < windows.length; i++){
+		var w = windows[i];
+		console.log(w);
+
+        // on click
+		w.addEventListener("mousedown", function(e){
+            // reset z-index of all windows to 10
+			activeWindow.style.zIndex = 10;
+            // get the window being clicked
+			activeWindow = e.target.closest('.window_container');
+            // bring to top (by setting z-index to 100)
+			activeWindow.style.zIndex = 100;
+		})
+		console.log(activeWindow)
+	}
+}
+
+init();
