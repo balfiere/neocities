aebdf77b1f005befeee1b7d593cca2c89194fcad
balfiere <deanleonheart@gmail.com>
2024-04-02 16:03:00 -0500

    add more pixels

--- public/changelog/evenwidth.js
+++ public/changelog/evenwidth.js
@@ -1,6 +1,6 @@
-$(window).on("load resize", function () {
+$(window).on("load", function () {
     $("#wrapper").css("width", "90%");
     $('#wrapper').each(function (index, value) {
-        if($(this).width()%4!=0) $(this).width( 4*Math.round($(this).width()/4 ) ) ;
+        if($(this).width()%2!=0) $(this).width( 2*Math.round($(this).width()/2 ) ) ;
     });
 })
