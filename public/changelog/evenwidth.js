1f2c80c5e5bfd4ca48e7cec2a1d6264d325c459f
balfiere <deanleonheart@gmail.com>
2024-04-24 03:16:46 -0500

    update changelog font and javascript

--- public/changelog/evenwidth.js
+++
@@ -1,6 +0,0 @@
-$(window).on("load", function () {
-    $("#wrapper").css("width", "90%");
-    $('#wrapper').each(function (index, value) {
-        if($(this).width()%2!=0) $(this).width( 2*Math.round($(this).width()/2 ) ) ;
-    });
-})
