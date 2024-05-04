4c567f123d7445d60ed27dd8811b1327f90611e4
balfiere <deanleonheart@gmail.com>
2024-05-04 11:56:35 -0500

    resources site overhaul. reformatted mobile navigation and removed most javascript

--- public/resources/scripts/toggle.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("open").addEventListener("click", function () {
-  document.getElementById('side').classList.toggle('open')
-  document.getElementById('open-icon').classList.toggle('close')
-  document.getElementById('open').classList.toggle('buttonclose')
-  return false;
-})
