7df9655a39a77fb054fcf1a59695cf8c942267ae
balfiere <42321564+balfiere@users.noreply.github.com>
2024-06-12 19:04:20 -0500

    add more buttons and cardcaptor sakura pixels

--- public/main/scripts/toggle.js
+++ public/main/scripts/toggle.js
@@ -1,5 +1,16 @@
-document.getElementById("open").addEventListener("click", function () {
+let burger = document.getElementById("open");
+
+// Setup our function to run on various events
+var toggleSidebar = function (event) {
+  // Do something...
     document.getElementById('leftcolumn').classList.toggle('open')
-    document.getElementById('open-icon').classList.toggle('close')
-    return false;
-  })
+    document.getElementById('ham4').classList.toggle('active')
+};
+
+burger.addEventListener('click', toggleSidebar, false);
+burger.addEventListener('keydown', (event) => {
+  let key = event.code;
+  if (key == "Escape" || key == "Enter") {
+      toggleSidebar(event);
+  }
+});
