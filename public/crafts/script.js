54731b11ca96d63754f3a6e4ed5279ef9975b031
balfiere <42321564+balfiere@users.noreply.github.com>
2025-09-06 20:32:34 -0500

    update notebook theme and overhauled craft page design

---
+++ public/crafts/script.js
@@ -0,0 +1,48 @@
+import { annotate } from './rough-notation.js';
+
+// hightlight effect on link hover
+const links = document.querySelectorAll("a");
+const linkAnnotateArray = [];
+links.forEach((link, i) => {
+    // if link is inside <header>
+        if (link.closest("header")) {
+            linkAnnotateArray.push(annotate(link, { type: "highlight", color: "rgba(255, 231, 233, 0.7)", padding: 15 }));
+        } else {
+            linkAnnotateArray.push(annotate(link, { type: "highlight", color: "rgba(255, 231, 233, 0.2)", padding: 15 }));
+        }
+    link.addEventListener("mouseover", () => {
+        linkAnnotateArray[i].show();
+    });
+    link.addEventListener("mouseout", () => {
+        linkAnnotateArray[i].hide();
+    });
+})
+
+// bracket effect on picture hover
+const pictures = document.querySelectorAll(".img-box");
+const captionAnnotateArray = [];
+pictures.forEach((picture, i) => {
+    const caption = picture.querySelector(".caption");
+    // if prefers dark mode
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        captionAnnotateArray.push(annotate(caption, { type: 'bracket', color: '#fff', strokeWidth: 4, padding: [10, 10], brackets: ['left', 'right'] }));
+    } else {
+        captionAnnotateArray.push(annotate(caption, { type: 'bracket', color: '#000', strokeWidth: 4, padding: [10, 10], brackets: ['left', 'right'] }));
+    }
+    picture.addEventListener("mouseover", () => {
+        captionAnnotateArray[i].show();
+    });
+    picture.addEventListener("mouseout", () => {
+        captionAnnotateArray[i].hide();
+    });
+})
+
+// close modal on esc key
+document.onkeydown = function(e) { e.keyCode === 27 && document.querySelector('.modal-toggle:checked + .modal .modal-close').click() }
+
+// add event listener (on change) to all toggles
+Array.prototype.slice.call(document.querySelectorAll('.modal-toggle')).forEach(function(toggle) { toggle.addEventListener('change', function(event) {
+  // allow only one checked toggle
+  Array.prototype.slice.call(document.querySelectorAll('.modal-toggle:not(#' + event.target.id + ')')).forEach(function(diff) { diff.checked = false })
+  // when modal is opened, add class to the body
+  document.body.className = event.target.checked ? 'modal-opened' : '' }) })
