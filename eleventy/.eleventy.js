adf955447e440c552247935b4ab6f27259dc814e
balfiere <42321564+balfiere@users.noreply.github.com>
2025-09-14 16:41:27 -0500

    test new site generation script

--- eleventy/.eleventy.js
+++ eleventy/.eleventy.js
@@ -2,31 +2,33 @@ const markdownIt = require('markdown-it');
 const markdownItAttrs = require('markdown-it-attrs');
 
 const mdOptions = {
-    html: true,
-    breaks: true,
-    linkify: true,
-    typographer: true
-  }
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true
+}
 
 const mdAttr = {
-    leftDelimiter: '{',
-    rightDelimiter: '}',
-    allowedAttributes: []  // empty array = all attributes are allowed
-  }
+  leftDelimiter: '{',
+  rightDelimiter: '}',
+  allowedAttributes: []  // empty array = all attributes are allowed
+}
 
 module.exports = async function (eleventyConfig) {
-    const { RenderPlugin } = await import("@11ty/eleventy");
-    eleventyConfig.addPlugin(RenderPlugin);
-    eleventyConfig.setLibrary(
+  const { RenderPlugin } = await import("@11ty/eleventy");
+
+  eleventyConfig.addPlugin(RenderPlugin);
+  eleventyConfig.setLibrary(
     'md',
     markdownIt(mdOptions)
       .use(markdownItAttrs, mdAttr)
   );
+
   eleventyConfig.addFilter("markdown", function (data) {
       return markdownIt.renderInline(data);
   });
+
   return {
-    markdownTemplateEngine: 'liquid',
     dataTemplateEngine: 'liquid',
     htmlTemplateEngine: 'liquid'
   };
