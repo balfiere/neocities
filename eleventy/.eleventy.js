5f8aacd6d28d2f16f758418489ded6036ae24320
balfiere <42321564+balfiere@users.noreply.github.com>
2025-09-14 12:56:27 -0500

    update eleventy to version 3

--- eleventy/.eleventy.js
+++ eleventy/.eleventy.js
@@ -1,8 +1,5 @@
 const markdownIt = require('markdown-it');
-// const markdownItAnchor = require('markdown-it-anchor')
-// const pluginTOC = require('eleventy-plugin-toc')
 const markdownItAttrs = require('markdown-it-attrs');
-// const markdownItContainer = require('markdown-it-container');
 
 const mdOptions = {
     html: true,
@@ -17,50 +14,16 @@ const mdAttr = {
     allowedAttributes: []  // empty array = all attributes are allowed
   }
 
-// const articleCont = {
-//         validate(params) {
-//           return params.trim().match(/article/)
-//         },
-//         render(tokens, idx) {  
-//           if (tokens[idx].nesting === 1) {
-//             // opening tag
-//             return '<article>\n';
-  
-//           } else {
-//             // closing tag
-//             return '</article>\n';
-//           }
-//         }
-//       }
-
-// const article = (children) => {
-//   const content = markdownIt.render(children);
-//   return `<article>${content}</article>`
-// }
-
-module.exports = eleventyConfig => {
-  // Markdown
-  eleventyConfig.setLibrary(
+module.exports = async function (eleventyConfig) {
+    const { RenderPlugin } = await import("@11ty/eleventy");
+    eleventyConfig.addPlugin(RenderPlugin);
+    eleventyConfig.setLibrary(
     'md',
     markdownIt(mdOptions)
       .use(markdownItAttrs, mdAttr)
-    //   .use(markdownItAnchor, mdAnchorOpts)
-      // .use(markdownItContainer, 'article', articleCont)
-      // .use(markdownItContainer, 'description')
   );
-  // eleventyConfig.addShortcode("divider", function() {
-  //   return `<div class="divider"><img src="divider2.gif"><img src="divider2.gif"><img src="divider2.gif"></div>`;
-  // });
-  // eleventyConfig.addPairedShortcode(
-  //   'description', (children) => {
-  //     return `<div class="description">${children}</div>`
-  //   });
-  // eleventyConfig.addPairedShortcode(
-  //   'article', (children) => {
-  //     return `<article>${children}</article>`
-  //   });
   eleventyConfig.addFilter("markdown", function (data) {
-	  return markdownIt.renderInline(data);
+      return markdownIt.renderInline(data);
   });
   return {
     markdownTemplateEngine: 'liquid',
