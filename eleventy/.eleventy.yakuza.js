640fbef75db4a5dc9ed3a2e5be5e48835e9cfdce
balfiere <42321564+balfiere@users.noreply.github.com>
2025-09-24 01:50:52 -0500

    change sort order

--- eleventy/.eleventy.yakuza.js
+++ eleventy/.eleventy.yakuza.js
@@ -4,14 +4,12 @@ module.exports = function (eleventyConfig) {
 
   core(eleventyConfig);
 
-  eleventyConfig.addPassthroughCopy("neocities/yakuza/characters/characters.css");
-  eleventyConfig.addPassthroughCopy("neocities/yakuza/characters/characters.js");
+  eleventyConfig.addPassthroughCopy("neocities/yakuza/characters/*.css");
+  eleventyConfig.addPassthroughCopy("neocities/yakuza/characters/*.js");
   
 	eleventyConfig.addCollection("yakuza-characters", function (collectionsApi) {
 		return collectionsApi.getFilteredByTag("yakuza-profiles").sort(function (a, b) {
-			// return a.inputPath.localeCompare(b.inputPath);
-      // sort by most recently edited first (the updated property)
-      return new Date(a.data.updated) - new Date(b.data.updated);
+      return new Date(b.data.updated) - new Date(a.data.updated);
 		});
 	});
 
